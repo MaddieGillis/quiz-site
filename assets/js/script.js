130ac49fcc8ec9b20f9d8a4bb6396271bf1d63ba
Maddie Gillis <madelinegillis@outlook.com>
2022-11-12 16:25:03 -0800

    added variables for the questions

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+
+//Questions for quiz
+
+//question one
+var questionOne = "What does JS stand for?";
+var qOneAnswerOne = "JavaScript";
+var qOneAnswerTwo = "Jump Start";
+var qOneAnswerThree = "Jerry Seinfield";
+var qOneAnswerFour = "Just Sing";
+
+//question two
+var questionTwo = "What does API stand for?";
+var qTwoAnswerOne = "American Pony Index";
+var qTwoAnswerTwo = "Application Programming Interface";
+var qTwoAnswerThree = "Are Pigeons Intelligent";
+var qTwoAnswerFour = "Always Point Inward";
+
+//question three
+var questionThree = "Which is an example of camel case?";
+var qThreeAnswerOne = "Camels are cute";
+var qThreeAnswerTwo = "camels-are-cute";
+var qThreeAnswerThree = "camelsAreCute";
+var qThreeAnswerFour = "CAMELSareCUTE";
+
+//question four
+var questionFour = "Who created JavaScript?";
+var qFourAnswerOne = "CC Tinsley";
+var qFourAnswerTwo = "Hank Anderson";
+var qFourAnswerThree = "Leigh Avidan";
+var qFourAnswerFour = "Brendan Eich";
+
+//question five
+var questionFive = "Which is not a primitive value?";
+var qFiveAnswerOne = "Number";
+var qFiveAnswerTwo = "Symbol";
+var qFiveAnswerThree = "Hunting and gathering";
+var qFiveAnswerFour = "String";
+
+
